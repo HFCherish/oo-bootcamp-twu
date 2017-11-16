d4f23a8d095a5c31528554b514c4948fcad81937
HFCherish <hf_cherish@163.com>
2017-11-16 16:07:44 +0800

    not replace others

--- 1_mommify/src/main/java/Mommy.java
+++ 1_mommify/src/main/java/Mommy.java
@@ -1,9 +1,19 @@
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * @author pzzheng
  * @date 11/16/17
  */
 public class Mommy {
+
+    public static final List<String> VOVELS = Arrays.asList("a", "e", "i", "o", "u");
+
     public String mommify(String a) {
-        return "mommyit";
+        if(VOVELS.contains(a)) {
+            return "mommy";
+        }
+
+        return a;
     }
 }
--- 1_mommify/src/test/java/MommyTest.java
+++ 1_mommify/src/test/java/MommyTest.java
@@ -1,6 +1,8 @@
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.nullValue;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 /**
@@ -8,14 +10,32 @@ import static org.hamcrest.MatcherAssert.assertThat;
  * @date 11/16/17
  */
 public class MommyTest {
+
+    private Mommy mommy;
+
+    @Before
+    public void setUp() {
+        mommy = new Mommy();
+    }
+
     /**
      * @author pzzheng
-     * @date ${DATE}
      */
     @Test
     public void should_replace_vowel() {
-        Mommy mommy = new Mommy();
-        String result = mommy.mommify("a");
-        assertThat(result, is("mommy"));
+        assertThat(mommy.mommify("a"), is("mommy"));
+        assertThat(mommy.mommify("e"), is("mommy"));
+        assertThat(mommy.mommify("o"), is("mommy"));
     }
+    /**
+     * @author pzzheng
+     */
+    @Test
+    public void should_not_replace_other_character() {
+        assertThat(mommy.mommify(""), is(""));
+        assertThat(mommy.mommify("b"), is("b"));
+        assertThat(mommy.mommify(" "), is(" "));
+        assertThat(mommy.mommify(null), is(nullValue()));
+    }
+
 }
